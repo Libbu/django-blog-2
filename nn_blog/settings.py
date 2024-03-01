a85826ec9d4037a687270b634d76440ffa3062ce
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-01 13:41:04 +0000

    migrated django default tables, connected db

--- nn_blog/settings.py
+++ nn_blog/settings.py
@@ -1,3 +1,10 @@
+import os
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
+
+
 """
 Django settings for nn_blog project.
 
@@ -74,12 +81,14 @@ WSGI_APPLICATION = 'nn_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
